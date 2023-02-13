b13dd0755dbf16bcb2e5304419ef0c8694691174
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-02-13 11:14:05 +0100

    TEst igen

--- tests/PokemonTest.php
+++ tests/PokemonTest.php
@@ -10,7 +10,7 @@ class PokemonTest extends TestCase{
         $pokemon = new Pokemon(5, "Pikachu");
         $this->assertSame(5, $pokemon->id);
         $this->assertSame("Pikachu", $pokemon->name);
-        $this->assertSame("https://img.pokemondb.net/sprites/bank/normal/" . strtolower("Pikachu") . ".png", $pokemon->getImageUrl() );        
+        $this->assertSame("TEST https://img.pokemondb.net/sprites/bank/normal/" . strtolower("Pikachu") . ".png", $pokemon->getImageUrl() );        
     }
 
 }
